5f4b506e242d61c81049a6b4a582a8b23c4b4794
devaiyaaz <mohammadaiyaz99@gmail.com>
2026-08-31 12:42:40 +0400

    Changes made in ChooseFruit function
    8 to 9

--- jquery.js
+++ jquery.js
@@ -122,7 +122,7 @@ function startAction(){
 // generate a random fruit
 
 function chooseFruit(){
-    $("#fruit1").attr('src' , 'images/' + fruits[Math.round(8*Math.random())] +'.png');   
+    $("#fruit1").attr('src' , 'images/' + fruits[Math.round(9*Math.random())] +'.png');   
 }
 
 //Stop dropping fruits
